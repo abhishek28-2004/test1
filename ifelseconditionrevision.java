b727649c463adc3e8bb8e50d79ab7faa36515234
abhishek28-2004 <yadavabhi4673@gmail.com>
2025-05-04 18:58:46 +0530

    Add files via upload

---
+++ ifelseconditionrevision.java
@@ -0,0 +1,16 @@
+package loop;
+import java.util.Scanner;
+public class ifelseconditionrevision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter a number ");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("the number i have entered is even");
+
+            }
+        else{
+            System.out.println("the number is odd");
+        }
+    }
+}
